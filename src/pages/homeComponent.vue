04b4e0c847712b4b8ff0cf7a0aa6576efc52b48b
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2023-04-21 11:25:26 +0700

    commit

--- src/pages/homeComponent.vue
+++ src/pages/homeComponent.vue
@@ -1,3 +1,55 @@
 <template>
   <h1>Overview is here</h1>
+  <input type="text" v-model="searchText" />
+  <ul>
+    <li v-for="(customer, index) in customersFilter" :key="index">
+      {{ customer }}
+    </li>
+  </ul>
 </template>
+<script>
+import { ref, reactive, computed, watch, watchEffect } from "vue";
+
+export default {
+  props: {
+    theme: {
+      type: String,
+      required: false,
+      default: "light",
+    },
+  },
+  // eslint-disable-next-line vue/no-setup-props-destructure
+  setup({ theme }, { emit }) {
+    console.log("props", theme);
+    console.log("context", emit);
+    const searchText = ref("");
+    const customers = reactive([
+      "Something",
+      "Nguyen Ba Tinh",
+      "Sky Albert",
+      "Hu la",
+    ]);
+
+    const customersFilter = computed(() =>
+      customers
+        .map((customer) => {
+          customer = customer.toLowerCase();
+          return customer;
+        })
+        .filter((customer) => customer.includes(searchText.value.toLowerCase()))
+    );
+
+    watch(searchText, (newValue, oldValue) => {
+      console.log(newValue, oldValue);
+    });
+
+    watchEffect(() => {
+      if (searchText.value) {
+        console.log("run again");
+      }
+    });
+
+    return { customersFilter, searchText };
+  },
+};
+</script>
